8e28fd542c2f7773bcf865d7b0f78587a2335fea
Rafał Stefański <rafal-stefanski@wp.pl>
2021-10-07 11:52:54 +0200

    Added dog facts with dogpic generator

---
+++ src/main/java/com/rafalstefanski/randomlistgenerator/dog/controller/FactsFromExternalApiController.java
@@ -0,0 +1,43 @@
+package com.rafalstefanski.randomlistgenerator.dog.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.rafalstefanski.randomlistgenerator.dog.model.DogFact;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+@RequestMapping("/dog-gui")
+public class FactsFromExternalApiController {
+
+    public FactsFromExternalApiController() {
+    }
+
+    private DogFact getDogFact() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // getting facts from external API, below one with dog facts from https://dukengn.github.io/Dog-facts-API/
+        DogFact[] forObject = restTemplate
+                .getForObject("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1",
+                        DogFact[].class);
+
+        // getting single data from API using JsonNode, used to get only image from JSON
+        JsonNode jsonNodeImage = restTemplate.getForObject("https://dog.ceo/api/breeds/image/random", JsonNode.class).get("message");
+
+
+        forObject[0].setSrc(jsonNodeImage.asText());
+        forObject[0].setInfo(String.valueOf(forObject[0].getAdditionalProperties().values()));
+
+        return forObject[0];
+    }
+
+    @GetMapping
+    public String get(Model model) {
+        model.addAttribute("dogFactImg", getDogFact());
+        return "dog-gui/dogView";
+    }
+
+
+}
---
+++ src/main/java/com/rafalstefanski/randomlistgenerator/dog/model/DogFact.java
@@ -0,0 +1,90 @@
+package com.rafalstefanski.randomlistgenerator.dog.model;
+
+import com.fasterxml.jackson.annotation.*;
+
+import javax.annotation.Generated;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "facts",
+    "success"
+})
+@Generated("jsonschema2pojo")
+public class DogFact {
+
+    @JsonProperty("facts")
+    private List<String> facts = null;
+
+    @JsonProperty("success")
+    private Boolean success;
+
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("facts")
+    public List<String> getFacts() {
+        return (List<String>) getAdditionalProperties();
+    }
+
+    @JsonProperty("facts")
+    public void setFacts(List<String> facts) {
+        this.facts = facts;
+    }
+
+    @JsonProperty("success")
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    @JsonProperty("success")
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    private String src;
+    private String info;
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public void setAdditionalProperties(Map<String, Object> additionalProperties) {
+        this.additionalProperties = additionalProperties;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    @Override
+    public String toString() {
+        return "DogFact{" +
+                "facts=" + facts +
+                ", success=" + success +
+                ", additionalProperties=" + additionalProperties +
+                ", src='" + src + '\'' +
+                ", info='" + info + '\'' +
+                '}';
+    }
+}
